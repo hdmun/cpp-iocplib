ffdca7ec92ee60d8263414d625919b8ad92bf19c
hdmun <huidae.mun@gmail.com>
2022-11-15 18:57:39 +0900

    signal 핸들러를 통한 graceful shutdown 구현
    - `SIGTERM` 윈도우에서 동작 안 하는듯?

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,39 @@
 ﻿#include "pch.h"
+#include <csignal>
+
 #include "iocplib/win_sock.h"
 
 static iocplib::WinSockInitializer g_winsock_init;
+static bool g_running = true;
+
+static void SignalHandler(int signo)
+{
+	switch (signo)
+	{
+	case SIGTERM:
+		g_running = false;
+		break;
+	case SIGINT:
+		g_running = false;
+		break;
+	default:
+		break;
+	}
+}
 
 int main()
 {
-	std::cout << "Hello Server!\n";
+	// https://tttsss77.tistory.com/212
+	if (signal(SIGTERM, SignalHandler) == SIG_ERR) {
+		return -1;
+	}
+	if (signal(SIGINT, SignalHandler) == SIG_ERR) {
+		return -1;
+	}
+
+	while (g_running) {
+		::Sleep(1000);
+	}
+
+	return 0;
 }
