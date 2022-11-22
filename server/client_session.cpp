9cf39f7c9847c98b71d4698abb8e9237cc749daa
hdmun <huidae.mun@gmail.com>
2022-11-22 17:58:55 +0900

    flatbuffers 샘플 서버 코드 구현

--- server/client_session.cpp
+++ server/client_session.cpp
@@ -1,5 +1,10 @@
 #include "pch.h"
 #include "client_session.h"
+#include "message/client_message_generated.h"
+
+#define PACKET_HANDLER(_TYPE, _HANDLER) \
+	case _TYPE: _HANDLER(packet->type_as##_HANDLER())
+
 
 ClientSession::ClientSession()
 {
@@ -10,9 +15,42 @@ void ClientSession::OnReceivePacket(uint8_t* buffer, uint32_t received)
 	uint32_t remain_length = received;
 	do {
 		remain_length = packet_receiver_.OnReceive(buffer, remain_length);
+		
 		if (packet_receiver_.IsComplete()) {
-			// deserialize
+			const auto* packet = message::client::GetPacket(packet_receiver_.buffer());
+			OnPacket(packet);
+
 			packet_receiver_.Clear();
 		}
 	} while (remain_length > 0);
+}
+
+void ClientSession::OnPacket(const message::client::Packet* packet)
+{
+	using msg = message::client::PacketType;
+
+	switch (packet->type_type())
+	{
+		PACKET_HANDLER(msg::PacketType_LoginRequest, _LoginRequest);
+		PACKET_HANDLER(msg::PacketType_ConnectServerRequest, _ConnectServerRequest);
+		PACKET_HANDLER(msg::PacketType_LogoutRequest, _LogoutRequest);
+	default:
+		break;
+	}
+}
+
+void ClientSession::_LoginRequest(const message::client::LoginRequest* request)
+{
+	request->id();
+	request->password();
+}
+
+void ClientSession::_LogoutRequest(const message::client::LogoutRequest* request)
+{
+	request;
+}
+
+void ClientSession::_ConnectServerRequest(const message::client::ConnectServerRequest* request)
+{
+	request->server_id();
 }
--- server/client_session.h
+++ server/client_session.h
@@ -3,6 +3,15 @@
 #include "iocplib/socket_session.h"
 #include "iocplib/packet.h"
 
+namespace message {
+	namespace client {
+		struct Packet;
+		struct LoginRequest;
+		struct LogoutRequest;
+		struct ConnectServerRequest;
+	}
+}
+
 class ClientSession
 	: public iocplib::SocketSession
 {
@@ -11,6 +20,13 @@ public:
 	virtual ~ClientSession() {}
 
 	virtual void OnReceivePacket(uint8_t* buffer, uint32_t received);
+
+private:
+	void OnPacket(const message::client::Packet* packet);
+
+	void _LoginRequest(const message::client::LoginRequest* request);
+	void _LogoutRequest(const message::client::LogoutRequest* request);
+	void _ConnectServerRequest(const message::client::ConnectServerRequest* request);
 
 private:
 	iocplib::PacketReceiver packet_receiver_;
