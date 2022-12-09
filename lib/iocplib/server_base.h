d5753b54e39d94b895d94ad4972af547dc517de9
hdmun <huidae.mun@gmail.com>
2022-12-09 19:59:17 +0900

    `iocplib/ServerBase` 클래스 코드 파일 하나로 병합

--- lib/iocplib/server_base.h
+++ lib/iocplib/server_base.h
@@ -8,11 +8,34 @@ namespace iocplib {
 	class ServerBase
 	{
 	public:
-		ServerBase();
+		ServerBase()
+			: acceptor_(std::make_unique<Acceptor<_Session> >())
+		{
+		}
+
 		virtual ~ServerBase() {}
 
-		bool Open(uint16_t port, uint32_t io_thread);
-		void Close();
+		bool Open(uint16_t port, uint32_t io_thread)
+		{
+			SOCKADDR_IN addr_in = { 0, };
+			addr_in.sin_family = AF_INET;
+			addr_in.sin_port = ::htons(port);
+			addr_in.sin_addr.s_addr = ::htonl(INADDR_ANY);
+
+			if (!acceptor_->Open(io_thread, &addr_in)) {
+				std::cout << "failed open accepter" << std::endl;
+				return false;
+			}
+
+			return true;
+		}
+
+		void Close()
+		{
+			if (acceptor_) {
+				acceptor_->Close();
+			}
+		}
 
 		ServerBase(const ServerBase&) = delete;
 		ServerBase operator=(const ServerBase&) = delete;
@@ -21,6 +44,4 @@ namespace iocplib {
 		std::unique_ptr<Acceptor<_Session> > acceptor_;
 	};
 }
-
-#include "src/server_base.cpp"
 #endif
