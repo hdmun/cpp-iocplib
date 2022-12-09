e30787ac6ea6288d61f99d54d12cf2a9ce8ef58a
hdmun <huidae.mun@gmail.com>
2022-12-09 20:22:26 +0900

    `iocplib/IoCompletionPortWorker` 클래스 코드 파일 하나로 병합
    - static 멤버는 `inline` 키워드 추가해서 초기화

--- lib/iocplib/io_completion_port.h
+++ lib/iocplib/io_completion_port.h
@@ -1,6 +1,7 @@
 #ifndef __IO_COMPLETION_PORT_H__
 #define __IO_COMPLETION_PORT_H__
 #include "../winsocklib/win_sock.h"
+#include "overlapped.h"
 
 namespace iocplib {
 	class IoCompletionPortWorker
@@ -9,11 +10,44 @@ namespace iocplib {
 		IoCompletionPortWorker(IoCompletionPortWorker&) = delete;
 		IoCompletionPortWorker operator= (IoCompletionPortWorker&) = delete;
 
-		IoCompletionPortWorker(HANDLE iocp_handle);
+		IoCompletionPortWorker(HANDLE iocp_handle)
+			: iocp_handle_(iocp_handle)
+			, thread_(&IoCompletionPortWorker::Run, this)
+		{
+		}
+
 		virtual ~IoCompletionPortWorker() {}
 
-		void Run();
-		void Join();
+		void Run()
+		{
+			while (true)
+			{
+				DWORD dwNumberOfBytesTransferred = 0;
+				ULONG_PTR completionKey;
+				LPOVERLAPPED lpOverlapped = nullptr;
+
+				BOOL bSuccess = ::GetQueuedCompletionStatus(
+					iocp_handle_,
+					&dwNumberOfBytesTransferred,
+					&completionKey,
+					&lpOverlapped,
+					INFINITE
+				);
+
+				if (!lpOverlapped) {
+					break;  // null 이면 스레드 종료 신호로 처리
+				}
+
+				DWORD dwError = bSuccess ? 0 : ::GetLastError();
+				OverlappedContext context = *reinterpret_cast<OverlappedContext*>(lpOverlapped);
+				context.callback->OnComplete(context.data, dwError, dwNumberOfBytesTransferred, completionKey);
+			}
+		}
+
+		void Join()
+		{
+			thread_.join();
+		}
 
 	private:
 		HANDLE iocp_handle_{ nullptr };
@@ -33,11 +67,66 @@ namespace iocplib {
 			return handle_;
 		}
 
-		bool Create(uint32_t io_thread = 0U);
-		void Close();
+		bool Create(uint32_t io_thread = 0U)
+		{
+			// IOCP 커널 오브젝트를 생성
+			handle_ = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+			if (!handle_) {
+				return false;
+			}
 
-		void Attach(HANDLE socket);
-		void Detach();
+			if (io_thread == 0U) {
+				SYSTEM_INFO si{ 0, };
+				::GetSystemInfo(&si);
+				io_thread = si.dwNumberOfProcessors * 2;
+			}
+
+			{
+				std::lock_guard<std::mutex> lock(worker_lock_);
+				workers_.reserve(io_thread);
+				for (uint32_t i = 0U; i < io_thread; i++) {
+					workers_.push_back(std::make_unique<IoCompletionPortWorker>(handle_));
+				}
+			}
+
+			return true;
+		}
+
+		void Close()
+		{
+			// wait socket handles
+			while (attached_sockets_ > 0) {
+				::YieldProcessor();
+			}
+
+			// wait worker thread
+			{
+				std::lock_guard<std::mutex> lock(worker_lock_);
+				for (int i = 0; i < workers_.size(); i++) {
+					::PostQueuedCompletionStatus(handle_, 0, 0, nullptr);
+				}
+
+				for (auto& worker : workers_) {
+					worker->Join();
+				}
+			}
+		}
+
+		void Attach(HANDLE socket)
+		{
+			// 소켓을 IOCP 핸들에 연결
+			if (!::CreateIoCompletionPort(socket, handle_, 0, 0)) {
+				__noop;  // exception?
+				return;
+			}
+
+			attached_sockets_++;
+		}
+
+		void Detach()
+		{
+			attached_sockets_--;
+		}
 
 	private:
 		HANDLE handle_{ nullptr };
@@ -45,7 +134,9 @@ namespace iocplib {
 		std::mutex worker_lock_;
 		std::vector< std::unique_ptr<IoCompletionPortWorker> > workers_;
 
-		static std::atomic<uint32_t> attached_sockets_;
+		inline static std::atomic<uint32_t> attached_sockets_{ 0 };
 	};
 }
+
+
 #endif
