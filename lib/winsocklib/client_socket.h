6c0135d7f4b6a41e4fb62e76b3bdef122af17537
hdmun <huidae.mun@gmail.com>
2022-12-09 19:55:35 +0900

    `winsocklib/ClientSocket` 클래스 코드 파일 하나로 병합

--- lib/winsocklib/client_socket.h
+++ lib/winsocklib/client_socket.h
@@ -12,23 +12,101 @@ namespace winsocklib {
 		: public std::enable_shared_from_this<ClientSocket<_Session> >
 	{
 	protected:
-		ClientSocket();
+		ClientSocket()
+			: sending_size_(0U)
+		{
+		}
+
 		virtual ~ClientSocket() {}
 
 	public:
-		bool Connect(const SOCKADDR_IN& addr_in);
-		void Close();
+		bool Connect(const SOCKADDR_IN& addr_in)
+		{
+			socket_.Create(AF_INET, SOCK_STREAM, 0);
+			socket_.SetNonBlocking(true);
+
+			io_thread_.RequestRegisterSocket(this->weak_from_this(), FD_READ | FD_WRITE | FD_CLOSE);
+
+			return socket_.Connect(reinterpret_cast<const SOCKADDR*>(&addr_in));
+		}
+
+		void Close()
+		{
+			io_thread_.RequestUnregisterSocket(this->weak_from_this());
+			sending_size_ = 0U;
+			socket_.Close();
+		}
 
 		// for SocketIoThreadCallback
 		const SOCKET GetSocketHandle() { return socket_.handle(); }
-		void OnSocketRead(int error);
-		void OnSocketWrite(int error);
-		void OnSocketClose(int error);
+		void OnSocketRead(int error)
+		{
+			if (error > 0) {
+				return;
+			}
 
-		void SendPacket(uint8_t* data, uint32_t size);
+			while (true) {
+				uint8_t* buffer = recv_buffer_ + recv_offset_;
+				int buffer_len = winsocklib::kSocketBufferSize - recv_offset_;
+				int received = socket_.Recv(reinterpret_cast<char*>(buffer), buffer_len, 0);
+				if (received <= 0) {
+					break;
+				}
+
+				uint32_t remain_length = received;
+				do {
+					remain_length = packet_receiver_.OnReceive(buffer, received);
+					if (packet_receiver_.IsComplete()) {
+						// on packet
+						reinterpret_cast<_Session*>(this)->OnPacket(packet_receiver_.buffer());
+						packet_receiver_.Clear();
+					}
+				} while (remain_length > 0);
+			}
+		}
+
+		void OnSocketWrite(int error)
+		{
+			if (error > 0) {
+				return;
+			}
+
+			TrySendPacket();
+		}
+
+		void OnSocketClose(int error)
+		{
+			Close();
+		}
+
+		void SendPacket(uint8_t* data, uint32_t size)
+		{
+			send_buffers_.push_back(iocplib::SocketBuffer::Allocate(data, size));
+
+			TrySendPacket();
+		}
 
 	private:
-		void TrySendPacket();
+		void TrySendPacket()
+		{
+			while (!send_buffers_.empty()) {
+				const auto& front = send_buffers_.front();
+
+				int send_len = front->GetBufferSize() - sending_size_;
+				int len = socket_.Send(front->buffer() + sending_size_, send_len);
+				if (len == 0) {
+					break;
+				}
+
+				sending_size_ += len;
+				if (sending_size_ < front->GetBufferSize()) {
+					continue;
+				}
+
+				send_buffers_.pop_front();
+				sending_size_ = 0;
+			}
+		}
 
 	private:
 		winsocklib::WinSock socket_;
@@ -45,7 +123,4 @@ namespace winsocklib {
 		uint32_t sending_size_;
 	};
 }
-
-#include "src/client_socket.cpp"
 #endif
-
