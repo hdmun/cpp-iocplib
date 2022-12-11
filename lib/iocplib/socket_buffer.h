318de9bebe179db42cadacb04d12e65e81f6b8ce
hdmun <huidae.mun@gmail.com>
2022-12-11 12:19:27 +0900

    `iocplib/SocketBuffer `, `iocp/PacketReceiver` 클래스 코드 파일 하나로 병합

--- lib/iocplib/socket_buffer.h
+++ lib/iocplib/socket_buffer.h
@@ -4,20 +4,50 @@
 namespace iocplib {
 	class SocketBuffer {
 	public:
-		SocketBuffer(uint8_t* data, uint32_t size);
-		SocketBuffer(std::vector<uint8_t>&& buffer);
-		SocketBuffer(SocketBuffer&& rhs);
+		SocketBuffer(uint8_t* data, uint32_t size)
+		{
+			constexpr uint32_t header_len = 4;
+			buffer_.resize(size + header_len);
+			std::copy(data, data + size, buffer_.begin() + header_len);
+			*reinterpret_cast<uint32_t*>(buffer_.data()) = size;
+		}
+
+		SocketBuffer(std::vector<uint8_t>&& buffer)
+			: buffer_(std::move(buffer))
+		{
+		}
+
+		SocketBuffer(SocketBuffer&& rhs)
+			: buffer_(std::move(rhs.buffer_))
+		{
+		}
 
 		virtual ~SocketBuffer() {}
 
-		static std::unique_ptr<SocketBuffer> Allocate(uint8_t* data, uint32_t size);
-		std::unique_ptr<SocketBuffer> Slice(int start, int size);
+		static std::unique_ptr<SocketBuffer> Allocate(uint8_t* data, uint32_t size)
+		{
+			return std::make_unique<SocketBuffer>(data, size);
+		}
 
-		uint8_t* buffer() {
+		std::unique_ptr<SocketBuffer> Slice(int start, int size)
+		{
+			if (buffer_.size() <= start + size) {
+				return nullptr;
+			}
+
+			const auto startIter = buffer_.begin() + start;
+			return std::make_unique<SocketBuffer>(
+				std::move(std::vector<uint8_t>(startIter, startIter + size))
+				);
+		}
+
+		uint8_t* buffer()
+		{
 			return buffer_.data();
 		}
 
-		uint32_t GetBufferSize() const {
+		uint32_t GetBufferSize() const
+		{
 			return static_cast<uint32_t>(buffer_.size());
 		}
 
